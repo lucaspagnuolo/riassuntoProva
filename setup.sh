542136f5d42fd58900f3d882fa8ff557fa3dccf5
lucaspagnuolo <luca.spagnuolo.ext@consip.it>
2025-04-15 12:56:20 +0200

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Installa pacchetti Python aggiuntivi se necessari
+pip install mistralai==0.1.7
+pip install python-docx
+
+# Eventuali altre installazioni di software di sistema (opzionali)
+# sudo apt-get install some-package
