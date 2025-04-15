12af3b549d6ae724a40ec3abdc4086d1df40c9c8
lucaspagnuolo <luca.spagnuolo.ext@consip.it>
2025-04-15 14:46:59 +0200

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Installa pacchetti Python aggiuntivi se necessari
-pip install mistralai==0.1.7
-pip install python-docx
-
-# Eventuali altre installazioni di software di sistema (opzionali)
-# sudo apt-get install some-package
